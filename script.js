89b9c70751d6ab750c0ee47ebd3b99a75db1f4cb
Samy <dev.exelight@gmail.com>
2021-11-21 21:57:07 +0100

    Form Validation functionalities

---
+++ script.js
@@ -0,0 +1,65 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+let passwordsMatch = false;
+
+const validateForm = () => {
+  // Using Contraint API
+  isValid = form.checkValidity();
+  // Style main message for an error
+  if (!isValid) {
+    message.textContent = "Please fill out all fields."
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+  // Check if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure Passwords match."
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+  // If form is valid and passwords match
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully Registered"
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
+}
+
+const storeFormData = () => {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value
+  }
+  // Do something with the user data
+  console.log(user);
+}
+
+const processFormData = (e) => {
+  e.preventDefault();
+  // Validate Form
+  validateForm();
+  // Submit Data if valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
+}
+
+// Event listener
+form.addEventListener('submit', processFormData);
